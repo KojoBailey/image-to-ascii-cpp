01e502423fdccb437b17f42baccb9b4f3e520c60
Kojo Bailey <kojomolojo@gmail.com>
2025-11-26 17:46:31 +0000

    fix clamp

--- src/image.cpp
+++ src/image.cpp
@@ -50,7 +50,7 @@ void Image::clamp(size_t max_width)
 		max_width = m_width;
 	}
 	/*const*/ size_t max_height =
-		std::round(static_cast<float>(m_height) * size_t_div(max_width, m_width));
+		std::round(static_cast<float>(m_height) * size_t_div(max_width, m_width)) / 2.0f;
 	const float x_step_size = size_t_div(m_width, max_width);
 	const float y_step_size = size_t_div(m_height, max_height);
 	std::cout << std::format("Row step size: {}, Column step size: {}\n", x_step_size, y_step_size);
@@ -59,14 +59,14 @@ void Image::clamp(size_t max_width)
 	// max_height = 20; //[!] remove
 	double y_step = 0;
 	for (size_t row = 0; row < max_height; row++) {
-		const size_t y_index = ((row+1) == max_height)
-			? max_height
+		const size_t y_index = (row+1) == max_height
+			? m_height-1
 			: std::floor(y_step);
 
 		double x_step = 0;
 		for (size_t column = 0; column < max_width; column++) {
-			const size_t x_index = ((column+1) == max_width)
-				? max_width
+			const size_t x_index = (column+1) == max_width
+				? m_width-1
 				: std::floor(x_step);
 			std::cout << std::format("{:03}:{:03} -> {:03}:{:03}\n", y_index, x_index, row, column);
 			compressed_data.push_back(m_data[x_index * y_index]);
