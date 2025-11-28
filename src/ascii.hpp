e97bfa31e0476fb805ba157cf3ea2ed70faa4ded
Kojo Bailey <kojomolojo@gmail.com>
2025-11-28 01:17:23 +0000

    Use HSL formula

--- src/ascii.hpp
+++ src/ascii.hpp
@@ -9,8 +9,13 @@ class ASCII {
 public:
 	static std::optional<char> rgb_to_symbol(const unsigned char r, const unsigned char g, const unsigned char b, const unsigned char a)
 	{
-		const float intensity = (r + g + b) / (255.0f * 3.0f) * a / 255.0f;
-		const size_t index = std::round(intensity * (symbols.size() - 1));
+		// Using HSL formula.
+		const float rn = r / 255.0f;
+		const float gn = g / 255.0f;
+		const float bn = b / 255.0f;
+		const float an = a / 255.0f;
+		const float lightness = (std::min({rn, gn, bn}) + std::max({rn, gn, bn})) / 2 * an;
+		const size_t index = std::round(lightness * (symbols.size() - 1));
 		if (index < 0 || index > 9) {
 			std::cout << std::format("Index: {}\n", index);
 			return {};
