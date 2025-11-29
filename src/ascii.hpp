ae20cb02067378125af342e117e3d3a61f604e2e
Kojo Bailey <kojomolojo@gmail.com>
2025-11-29 18:52:20 +0000

    use lambda to abstract repetitive operation

--- src/ascii.hpp
+++ src/ascii.hpp
@@ -10,10 +10,13 @@ public:
 	static std::optional<char> rgb_to_symbol(const unsigned char r, const unsigned char g, const unsigned char b, const unsigned char a)
 	{
 		// Using HSL formula.
-		const float rn = r / 255.0f;
-		const float gn = g / 255.0f;
-		const float bn = b / 255.0f;
-		const float an = a / 255.0f;
+		std::function<float(const unsigned char)> rgb_byte_to_float = [](const unsigned char channel) {
+			return channel / 255.0f;
+		};
+		const float rn = rgb_byte_to_float(r);
+		const float gn = rgb_byte_to_float(g);
+		const float bn = rgb_byte_to_float(b);
+		const float an = rgb_byte_to_float(a);
 		const float lightness = (std::min({rn, gn, bn}) + std::max({rn, gn, bn})) / 2 * an;
 		const size_t index = std::round(lightness * (symbols.size() - 1));
 		if (index < 0 || index > 9) {
